78cf96ca2cecfe440a9654d4b6c11a3f6f60c59e
rafiul-2001 <rafiulislam2001@gmail.com>
2025-04-13 23:01:10 +0600

    #

--- readMEitsImportant.js
+++ readMEitsImportant.js
@@ -1,5 +1,14 @@
+//forgot all "_" just use : document.querySelector("")
+//for class use css style like querySelector(".className")
+//for id querySelector("#id")
+// if multiple name in same boundary then it will only work for the first one.
+//if a tag is inside another tag then it will be querySelector("1stTagOrClassOrID 2ndTag"); 
+
+
+
 //const variableName = document.getElementById("");
-// variableName.innerHTML = "Changes what u want"; what is that?
+// variableName.innerHTML = "Changes what u want"; 
+//or directly changes: document.getElementById("").innerHTML = "ur changes";
 // document: what we see in a webpage  as a whole is called document
 
 
