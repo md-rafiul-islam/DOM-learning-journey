0a599097c9b5aec3b6d78bb12f6ffb85e8256599
rafiul-2001 <rafiulislam2001@gmail.com>
2025-04-13 22:06:46 +0600

    #

---
+++ readMEitsImportant.js
@@ -0,0 +1,11 @@
+//const variableName = document.getElementById("");
+// variableName.innerHTML = "Changes what u want"; what is that?
+// document: what we see in a webpage  as a whole is called document
+
+
+
+
+// if multiple class has same name or a banch of collection have same name then choose its index no
+// same goes for tagName but always have to mention index no , even though it has one tag
+//document.getElementsByClassName("123")[0].innerHTML= "changes u want" //class name
+//document.getElementsByTagName("p")[1].innerHTML = "changes"; // for tag
